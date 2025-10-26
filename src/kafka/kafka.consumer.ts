eaec55e4189ce43e83f52fdb1dc75859311d6fad
ErickHenrique <erick.2henriquehtp@gmail.com>
2025-10-26 18:04:38 -0300

    Add Kafka producer to handle order validation and send messages based on stock update results

--- src/kafka/kafka.consumer.ts
+++ src/kafka/kafka.consumer.ts
@@ -9,11 +9,13 @@ export class KafkaConsumerService implements OnModuleInit {
     brokers: ['orderservice-kafka-1:9092'],
   });
   private readonly consumer = this.kafka.consumer({ groupId: 'product-service-group' });
+  private readonly producer = this.kafka.producer();
 
   constructor(private readonly productsService: ProductsService) {}
 
   async onModuleInit() {
     await this.consumer.connect();
+    await this.producer.connect();
     await this.consumer.subscribe({ topic: 'order_created', fromBeginning: true });
 
     await this.consumer.run({
@@ -28,13 +30,29 @@ export class KafkaConsumerService implements OnModuleInit {
   }
 
   private async handleOrderCreated(event: any) {
-    const { products } = event;
+    const { orderId, products } = event;
+    let valid = true;
+
     for (const item of products) {
       try {
         await this.productsService.decreaseStock(item.productId, item.quantity);
       } catch (error) {
-        console.error(`Erro ao atualizar estoque do produto ${item.productId}:`, error.message);
+        valid = false;
+        console.error(`Erro no estoque do produto ${item.productId}: ${error.message}`);
+        break;
       }
+    }
+
+    if (valid) {
+      await this.producer.send({
+        topic: 'order_validated',
+        messages: [{ value: JSON.stringify({ orderId, products }) }],
+      });
+    } else {
+      await this.producer.send({
+        topic: 'order_invalid',
+        messages: [{ value: JSON.stringify({ orderId, products }) }],
+      });
     }
   }
 }
